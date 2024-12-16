480d1ac09bafc9111165393f99579792e4683cbc
stammer1 <stammer786@gmail.com>
2024-12-16 09:54:23 -0800

    Location and Temperature is setted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NgIf],
+  imports: [RouterOutlet , NgIf , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
