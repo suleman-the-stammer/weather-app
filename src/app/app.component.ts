476c0cf61854a8a71a4403602b1eb234a7bcd3b3
stammer1 <stammer786@gmail.com>
2024-12-19 15:03:42 -0800

    Adding Search Funcationality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,54 @@ import { error } from 'console';
 import { WeatherService } from './services/weather.service';
 import { response } from 'express';
 import { WeatherData } from './model/weather.model';
+import { FormsModule, NgForm } from '@angular/forms';
 
 
+// @NgModule({
+//   declarations: [
+//     // Your components
+//   ],
+//   imports: [
+//      NgForm
+//   ]
+ 
+// })
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NgIf , CommonModule],
+  imports: [RouterOutlet , NgIf ,   CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 
 export class AppComponent implements OnInit{
  constructor(private weatherService : WeatherService){
 
  }
+ cityName: string = 'Lahore';
  
   weatherData?: WeatherData;
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Karachi')
+    this.getWeatherData(this.cityName);
+      
+  }
+  onSubmit(){
+   this.getWeatherData(this.cityName);
+   this.cityName=' ';
+  }
+
+  private getWeatherData(cityName: string){
+    this.weatherService.getWeatherData(cityName)
     .subscribe({
       next: (response) =>{
         this.weatherData = response;
         console.log(response);
       }
     })
-      
   }
 } 
   
